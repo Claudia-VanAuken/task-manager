82ca5d6247c6079d132e038e6a0f71834e51880b
Claudia VanAuken <claudiavanauken@gmail.com>
2026-02-12 12:41:14 -0600

    Basic task manager with add and view functions

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-console.log("Hello Git") 
-console.log("Task manager started");
+const readline = require('readline-sync');
+
+let taskList = [];
+let continuePrompt = true;
+
+console.log("Welcome to your Task Manager!");
+
+while (continuePrompt) {
+   let newTaskPrompt = readline.question("Please type 'View' to view your list, 'Add' to add to the list, Or type 'Exit' to quit.");
+
+   if (newTaskPrompt === 'Exit' || newTaskPrompt === 'exit'){
+      continuePrompt = false;
+      console.log("Have a great day!");
+   }
+
+   else if (newTaskPrompt === 'View' || newTaskPrompt === 'view'){
+      console.log("Got it! Your current list is... ", taskList);
+
+   }
+
+   else if (newTaskPrompt === 'Add' || newTaskPrompt === 'add'){
+      let newTask = readline.question('What task would you like to add?');
+      taskList.push(newTask);
+      console.log("Got it! Your current list is... ", taskList);
+
+}
+   else {
+      
+      console.log("I'm sorry! Please enter either 'View', 'Add', or 'Exit'");
+   }
+        
+}
